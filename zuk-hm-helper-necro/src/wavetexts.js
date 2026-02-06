1e79b934d3573538a0f1b13023fb4cf2d3640d08
DrewBFG <drew@blueforcegear.com>
2026-02-05 22:50:53 -0500

    Alpha upload for Zuk HM Helper Necro. Only text has been changed to match PVME's rotations.

---
+++ zuk-hm-helper-necro/src/wavetexts.js
@@ -0,0 +1,212 @@
+export const wave_text = {
+"1": `
+(Go East) :conjurearmy: → :lifetransfer: → :commandghost: → (East Meleer) :soulsap: → :touchofdeath: →
+
+(Go North) :livingdeath: + :adrenrenewal: → :threadsoffate: → (Any North Meleer) :soulsap: → :volleyofsouls: →
+
+:surge: (South) + :omniguard: :spec: (Kill both bats with :threadsoffate: + :omniguard: :spec:) → :invokedeath: → :commandskeleton:
+
+(Go North East for Wave 2)
+    `,
+"2": `
+Start North-East
+
+(NE Ranger) :deathspark: + :soulsap: → :surge: (West and tc NW Ranger) + :deathskulls: →
+
+:dive: (East and tc NE Meleer) + :deathspark: → :soulsap: →
+
+(Move South) :fingerofdeath: → (West Meleer) :deathspark: → (Yt-MejKot) :deathskulls: → finish with basics → :invokedeath: → :commandskeleton:
+
+(Go South East for Wave 3)
+`,
+"3": `
+Start South-East
+
+(SE Yt-MejKot) :bloat: → :fingerofdeath: → (Bat) :soulsap: → :touchofdeath: →
+
+(Move West) :invokedeath: → :threadsoffate: → :soulsap: → :volleyofsouls: →
+
+:surge: (East) + :deathguard90: :eofspec: (East Ranger) → :lifetransfer: → :invokedeath: → :commandskeleton:
+
+(Go South East for Wave 4 Igneous Mej)
+`,
+"4": `
+⬥ Igneous Mej: (:commandskeleton: → :invokedeath:) → :vulnbomb: + :bloat: → :vulnbomb: → :soulsap: → :touchofdeath: → :surge: towards Igneous Xil + :invokedeath:
+
+⬥ Igneous Xil: :vulnbomb: + :bloat: → :soulsap: → :necroauto: → :commandskeleton:
+
+⬥ Igneous Hur (Kite to Zuk): :invokedeath: → :vulnbomb: + :soulstrike: → :soulsap: → :splitsoul: → :necroauto: → :soulsap:
+
+⬥ Zuk :tzkalzuk:: :vulnbomb: + :deathskulls: → :touchofdeath: → :soulsap: → :volleyofsouls: → :deathguard90: :eofspec: → Dismiss conjures and :conjurearmy: → :invokedeath:
+`,
+"5": `
+:lifetransfer: → :commandghost: before the challenge monsters spawn (can do this after the challenge monsters die if preferred)
+
+:threadsoffate: → :tc: + :soulsap: → :volleyofsouls: → (target the two that didn't get hit) → :omniguard: :spec:.
+
+(Go North for Wave 6)
+`,
+"6": `
+Start North
+
+:commandghost: → :commandskeleton: → (North Ranger) :deathspark: → :soulsap: →
+
+:surge: (South and tc Jad) + :touchofdeath: → :vulnbomb: + :livingdeath: + adrenrenewal: → :touchofdeath: →
+
+:surge: (towards entrance) + :deathskulls: → :invokedeath: → :fingerofdeath: → (Bats) :soulsap: → :deathspark: → :necroauto: → :soulsap: → (Jad) :fingerofdeath: →
+
+:surge: (North after Jad dies) + :deathskulls: (Meleer) → finish Rangers with :fingerofdeath: / :necroauto: / :soulsap: → :lifetransfer: → :invokedeath: → :commandskeleton:
+
+(Go East for Wave 7)
+`,
+"7": `
+Start East
+
+(East Bat) :bloat: → (North Mager) :bloat: → (West Bat) :fingerofdeath: → (NW Ranger) :volleyofsouls: → (NE Ranger) :bloat: → :soulsap: → (East Ranger) :invokedeath: → :bloat: → :conjurearmy: → :invokedeath: → :commandghost: →
+
+:dive: (Middle) + :threadsoffate: → :soulsap: → :volleyofsouls: → :lifetransfer: → :invokedeath: → :commandskeleton:
+
+(Go East for Wave 8)
+`,
+"8": `
+Start East
+
+(East Mager) + :bloat: → (North Ranger) :soulsap: → :touchofdeath: → Go NW (West Mager) :deathskulls: → :soulsap: → :commandskeleton: →
+
+:surge: (East) + :invokedeath: → (NE Ranger) :bloat: → (Move behind east rock) :invokedeath: → (East Ranger) :bloat: → :conjurearmy: → :invokedeath: → :commandghost: →
+
+:dive: (Middle) + :threadsoffate: → :soulsap: → :volleyofsouls: → :lifetransfer: → :invokedeath: → :commandskeleton:
+
+(Go South East for Wave 9 Igneous Mej)
+
+Notes:
+
+⬥ Ensure you do not walk out of range of the TzekHaar-Mej after casting :deathskulls: or you will lose the bounce
+
+⬥ The third :soulsap: can be cast on any of the mobs in the middle, the TzekHaar-Yt-MejKot is often the ideal target. Ensure you do not walk too far out or else the :threadsoffate: + :volleyofsouls: will not hit everything
+`,
+"9": `
+⬥ Igneous Mej: (:commandskeleton: → :invokedeath:) → :vulnbomb: + :bloat: → :vulnbomb: → :soulsap: → :touchofdeath: → :surge: towards Igneous Xil + :invokedeath:
+
+⬥ Igneous Xil: :vulnbomb: + :bloat: → :soulsap: → :necroauto: → :commandskeleton:
+
+⬥ Igneous Hur (Kite to Zuk): :invokedeath: → :vulnbomb: + :soulstrike: → :soulsap: → :splitsoul: → :necroauto: → :soulsap:
+
+⬥ Zuk :tzkalzuk:: :vulnbomb: + :deathskulls: → :bloat: → :soulsap: → :touchofdeath: → :volleyofsouls: → :omniguard: :spec: → dismiss conjures + :conjurearmy: → :invokedeath:
+`,
+"10": `
+(tc) + :vulnbomb: + :deathspark: → :deathguard90: :eofspec: → :commandghost: → :lifetransfer: → :commandskeleton:
+
+(Go North for Wave 11)
+`,
+"11": `
+Start North
+
+(North Mager) :bloat: → :surge: (South) → (SE Bat) :soulsap: → :touchofdeath: →
+
+:surge: (West) → (SW Bat) :deathspark: → :soulsap: → (Go NW rock) :invokedeath: → :livingdeath: + :adrenrenewal: → (Jad) :touchofdeath: → (NW Mager) :deathskulls: → (West Mager) :soulsap: → :fingerofdeath: → (Jad) :necroauto: → :necroauto: → :fingerofdeath: → :invokedeath: →
+
+:dive: (North) + :threadsoffate: → :soulsap: → :volleyofsouls: → :deathskulls: → (Yt-MejKot) :fingerofdeath: → finish with basics → :lifetransfer: → :invokedeath: → :commandskeleton:
+
+(Go North for Wave 12)
+`,
+"12": `
+Start North
+
+(Bat) + :bloat: → (Move East) :invokedeath: → (East Ranger) :bloat: → :invokedeath: → (East Mager) :soulsap: → :necroauto: → :surge: (SW) → (SW Mager) :soulsap: → :touchofdeath: → :necroauto: → (Bat) :commandskeleton: → :soulsap: →
+
+:invokedeath: → :dive: (Middle) + :threadsoffate: → :soulsap: → :volleyofsouls: → :invokedeath: → :lifetransfer: → :commandskeleton:
+
+(Go North for Wave 13)
+
+Note: Ensure the East ranger and mager are dead before :invokedeath: and moving to the middle to :threadsoffate:
+`,
+"13": `
+Start North
+
+(North Ranger) :bloat: → (East Mager) :invokedeath: → :bloat: → (SE Ket-Zek) :deathskulls: →
+
+:surge: + :dive: (SW) → :invokedeath: → (SW Ket-Zek) :soulsap: → :touchofdeath: → :necroauto: → :commandzombie: → :conjurearmy: → :soulsap: → :commandghost: → (South Ranger) :commandskeleton: →
+
+finish South mager and ranger with basics :threadsoffate: when cd is <3s → :invokedeath: → :dive: (Middle) + :soulsap: → :volleyofsouls: → :invokedeath: → :commandskeleton:
+
+(Go South East for Wave 14 Igneous Mej)
+`,
+"14": `
+⬥ Igneous Mej: (:commandskeleton: → :invokedeath:) → :vulnbomb: + :bloat: → :vulnbomb: → :soulsap: → :touchofdeath: → :surge: towards Igneous Xil + :invokedeath:
+
+⬥ Igneous Xil: :vulnbomb: + :bloat: → :soulsap: → :necroauto: → :commandskeleton:
+
+⬥ Igneous Hur (Kite to Zuk): :invokedeath: → :vulnbomb: + :deathguard90: :eofspec: → :soulsap: → :splitsoul: → :touchofdeath:
+
+⬥ Zuk :tzkalzuk:: :vulnbomb: + :deathskulls: → :soulsap: → :commandzombie: → :commandskeleton: → :soulsap: → :volleyofsouls: → :touchofdeath: → dismiss conjures + :conjurearmy: → :lifetransfer:
+`,
+"15": `
+:disrupt: + :commandghost: → :devo: → :soulsap: → :necroauto: → :livingdeath: + :adrenrenewal: → :soulsap: → :touchofdeath: → :divert: → :soulsap: → :invokedeath: → :commandskeleton:
+
+(Go to North West Rock for Wave 16 Triple Jad)
+
+Notes:
+:disrupt: is used to block the first grey hit, :devo: + either :deflectrange: (green) or :deflectmage: (blue) are used for the next 2 hits and :divert: to block the final grey hit. Using :livingdeath: here ensures it is back in time for :tzkalzuk:.
+`,
+"16": `
+Stand beside the north-west rock and kill the north-west Jad while the other 2 group
+
+Jad 1: :vulnbomb: + :deathskulls: → :soulsap: → :volleyofsouls: → :touchofdeath: → :invokedeath: (When Jad 1 dies)
+
+Jad 2 and 3: :threadsoffate: → :fingerofdeath: → :deathskulls: → :fingerofdeath: → finish with basics → build :residualsoul: and :necrosis: on :tzkalzuk:
+
+:invokedeath: → :splitsoul: → :commandwarrior: pre Har-Aken
+`,
+"17": `
+:dive: to :haraken: + :bloat: → :soulsap: → :omniguard: :spec: → :deathspark: → :soulsap: → :deathskulls: → :commandskeleton: → :soulsap: (if 5 residualsoul then :volleyofsouls: ) → :deathspark: → :touchofdeath: → :soulsap: → :deathspark: → :commandzombie: → finish with basics → dismiss conjures when :haraken: dies
+`,
+"18": `
+First cycle
+Build to 100% adren pre fight by using basics on Zuk :tzkalzuk: while he is on his throne
+
+⬥ Apply :vulnbomb:
+⬥ Use :Veng:
+⬥ Equip :firstnecrohandwrap: and :zorgothsring:
+
+:conjurearmy: → :soulsap: → :commandghost: → :nat: → build to 100% adrenaline with :soulsap: → :touchofdeath: → :necroauto: → :soulsap: → :livingdeath: → :touchofdeath: → :soulsap: → :deathskulls: → :freedom: (bleed) → :divert: → :bloat: → :commandskeleton: → :splitsoul: → :volleyofsouls: → :surge: + :dive: (debuff) + :deathskulls: → :fingerofdeath: → :soulsap: → :touchofdeath: → :surge: (slam) + :fingerofdeath: → :soulsap: → :necroauto: → :commandskeleton: → :deathskulls: + :disrupt: (big hit) → :soulsap: → :omniguard: :spec: → :deathspark: → :touchofdeath: → :deathguard90: :eofspec: → :volleyofsouls: → :soulsap: → :freedom:
+
+Note: Some improvisation will be necessary as it depends on when you started attacking Zuk (and by extension when you get melee slammed) and how much adrenaline you have
+
+Pizza Phase
+Move with :surge: + :dive: between each monster to avoid the fire
+
+Igneous Hur: :invokedeath: → :surge: :dive: → :vulnbomb: + :soulstrike: → :soulsap: → :deathspark: → :commandskeleton: → :soulsap:
+
+Igneous Xil: :invokedeath: → :surge: :dive: → :vulnbomb: + :bloat: → :soulsap: → :touchofdeath: → :deathspark: → :soulsap: → :conjurearmy: → :lifetransfer: → :commandghost:
+
+Igneous Mej: :invokedeath: → :surge: :dive: → :bloat: → :soulsap: → :touchofdeath: → :necroauto: → :soulsap:
+
+Second cycle
+:Veng: + :livingdeath: + :adrenrenewal: → :touchofdeath: + :vulnbomb: → :deathskulls: + click extra-action button → :divert: → :splitsoul: → :volleyofsouls: → :fingerofdeath: → :commandskeleton: → :freedom: (bleed) → :deathskulls: → :touchofdeath: → :fingerofdeath: → :necroauto: → :fingerofdeath: → :surge: + :dive: (debuff) + :soulsap: → :necroauto: → :necroauto: → :deathskulls: → :commandskeleton: → :surge: (slam) + :soulsap: → :omniguard: :spec: → :soulsap: → :deathspark:
+
+Note: Again, some improvisation is required here as it depends on when you started attacking Zuk (and where you get melee slammed in your rotation) and on adrenaline
+
+Conduits
+This assumes :livingdeath: has ran out by the time you get conduits. If this isn't the case you can use an extra :deathskulls: before :livingdeath: ends on the conduit.
+
+⬥ Deal with Zuk's charged attacks using the following:
+
+  • C1 → :disrupt:
+  • C2 → :divert: / :res:
+  • C3 → :reflect:
+  • C4 → :powerburstofvitality:
+
+Conduit 1: :vulnbomb: + :invokedeath: → :touchofdeath: → :deathspark: → :deathskulls: → :deathspark:
+
+Conduit 2: :vulnbomb: + :invokedeath: → :bloat: → :soulsap: → :commandskeleton: → :commandzombie: → :soulsap:
+
+Conduit 3: :vulnbomb: + :invokedeath: → :splitsoul: → :volleyofsouls:
+
+Conduit 4: :vulnbomb: + :invokedeath: → :bloat: → :deathguard90: :eofspec: → fillers to finish
+`,
+
+
+
+
+}
